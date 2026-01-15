e28e0313fe603fd42e45b24b06bf193c174ea343
ShivamK14 <shivam.karle@gmail.com>
2026-01-15 20:08:02 +0530

    changes

--- LogicareIndia_backup_20260115_142221/src/utils/cn.js
+++
@@ -1,7 +0,0 @@
-/**
- * Utility function to merge class names
- * Helps with conditional classes and prevents issues with undefined/null values
- */
-export function cn(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
