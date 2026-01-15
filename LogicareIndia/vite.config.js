7b50201fc3bb3cca6e8cdc1197579fbe1b480c58
ShivamK14 <shivam.karle@gmail.com>
2026-01-15 20:16:10 +0530

    vite.jschnage

--- LogicareIndia/vite.config.js
+++ LogicareIndia/vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: process.env.PORT || 5173,
     strictPort: true,
   },
   preview: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: process.env.PORT || 4173,
     strictPort: true,
     allowedHosts: [
-      "logicare.onrender.com",
-      ".onrender.com", // Allow all Render subdomains
+      'logicare.onrender.com',
+      '.onrender.com', // Allow all Render subdomains
     ],
   },
-});
+})
